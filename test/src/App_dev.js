8cea015238770c814c6bf29ebf06249a14d00818
nosrangsiman <rangsiman.panthet@gmail.com>
2022-06-22 15:06:41 +0700

    Dockerfile.dev

--- test/src/App_dev.js
+++ test/src/App_dev.js
@@ -22,7 +22,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is workshop react-docker-cicd</h1>
         <p>
-          presented by {userName}
+          presented by {userName} .dev
         </p>
       </header>
       
